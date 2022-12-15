f156634c186acbeb734fe05dc9dbb2cb5419c005
Serena Black <sunsetsin1.sy@gmail.com>
2022-12-15 14:47:57 +0545

    feast: post api to add vaccine

---
+++ server/models/vaccine.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const vaccineSchema = new mongoose.Schema({
+  vaccineName: {
+    type: String,
+    required: true,
+  },
+  manufacturingCompany: {
+    type: String,
+    required: true,
+  },
+  companyEmail: {
+    type: String,
+    required: true,
+  },
+  companyContact: {
+    type: String,
+    required: true,
+  },
+  manufacturedDate: {
+    type: Date,
+    required: true,
+  },
+  numberOfDose: {
+    type: Number,
+    required: true,
+  },
+  vaccineRoute: String,
+  vaccinationAge: String,
+  vaccineImage: String,
+});
+
+vaccineSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Vaccine", vaccineSchema);
