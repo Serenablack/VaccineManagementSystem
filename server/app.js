c483ef8c3fa9fdc624770a1e1ed11f857b9b33ce
Serena Black <sunsetsin1.sy@gmail.com>
2022-12-15 13:16:23 +0545

    feat: middleware

--- server/app.js
+++ server/app.js
@@ -5,9 +5,12 @@ const path = require("path");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const vaccineRouter = require("./controllers/vaccineRouter");
+const usersRouter = require("./controllers/usersRouter");
+const loginRouter = require("./controllers/loginRouter");
+const middleware = require("./utils/middleware");
 
 mongoose
-  .connect(config.MongoDB)
+  .connect(config.DATABASE_URL)
   .then(() => {
     console.log("connected to MongoDB");
   })
@@ -17,9 +20,14 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
+app.use(middleware.userExtractor);
 app.use(express.static("dist"));
-app.use("/api/vaccine", vaccineRouter);
-// sends index.html
+app.use("/api/vaccines", vaccineRouter);
+
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
